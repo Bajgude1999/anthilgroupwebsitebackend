7052bc65582d2a6afc87faf069cc5d3acb64cfbb
Bajgude1999 <bajgudeyogesh4@gmail.com>
2026-04-02 23:19:36 +0530

    103

--- src/main/java/com/anthilgroup/anthilgroupwebsite/mail/controller/EnquiryController.java
+++ src/main/java/com/anthilgroup/anthilgroupwebsite/mail/controller/EnquiryController.java
@@ -1,6 +1,7 @@
 package com.anthilgroup.anthilgroupwebsite.mail.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.anthilgroup.anthilgroupwebsite.mail.dto.EnquiryDTO;
 import com.anthilgroup.anthilgroupwebsite.mail.service.IEmailService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/enquiry")
 public class EnquiryController {
